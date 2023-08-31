3b76a57349a7109bd5ef6a681b7f8ac1964df413
DerVogel101 <128903814+DerVogel101@users.noreply.github.com>
2023-08-31 19:21:44 +0200

    Update launch.sh
    added venv

--- launch.sh
+++ launch.sh
@@ -8,6 +8,14 @@ else
     echo "API_KEY=\"$api_key\"" > .env
 fi
 
+if [ -d venv ]; then
+    echo "Found venv."
+else
+    echo "Creating venv..."
+    python3 -m virtualenv ./venv
+fi
+source venv/bin/activate
+
 echo "Installing Python packages..."
 pip install -r requirements.txt
 
